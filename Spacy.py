0e6d45e6f8ff7a7150da07c7352e34cfde26a4f6
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 02:43:06 -0300

    Identificar classes da palavra com Spacy

--- Spacy.py
+++ Spacy.py
@@ -6,10 +6,13 @@ Pacote de frases: python -m spacy donwload pt_core_news_sm
 """
 import spacy
 nlp = spacy.load('pt_core_news_sm')
+class_pt = {'ADJ': 'adjetivo', 'ADP' : 'aposição', 'ADV': 'adverbio', 'AUX': 'auxiliar', 'CCONJ': ['conjunção', 'coordenativa'], 'DET': 'determinante', 'INTJ' : 'interjeição', 'NOUN' : 'substantivo', 'NUM' : 'numeral', 'PART': 'partícula', 'PRON' : 'pronome', 'PROPN' : 'nome próprio', 'PUNCT' : 'pontuação', 'SCONJ' : 'conjunção subordinativa', 'SYM' : 'símbolo', 'VERB' : 'verbo', 'X' : 'outro'}
 
-doc = nlp('A galinha atravessou a rua para ganhar um milhão.')
-word_With_Classe = {}
+doc = nlp('A galinha atravessou a rua para ganhar um milhão.') #Coloque o texto aqui
+
+word_With_Class = {}
 for token in doc:
-    word_With_Classe[token.text] = spacy.explain(token.tag_)
+    Class = token.tag_
+    word_With_Class[token.text] = class_pt[Class]
 
-print(word_With_Classe)
+print(word_With_Class)
