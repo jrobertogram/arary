766d2cb9bae3321b15a6f4bd948ca552c7718b19
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 02:49:31 -0300

    Function spacy criada

--- Spacy.py
+++ Spacy.py
@@ -5,14 +5,15 @@ Pacote de frases: python -m spacy donwload pt_core_news_sm
               ou: python -m spacy donwload pt_core_news_md
 """
 import spacy
-nlp = spacy.load('pt_core_news_sm')
-class_pt = {'ADJ': 'adjetivo', 'ADP' : 'aposição', 'ADV': 'adverbio', 'AUX': 'auxiliar', 'CCONJ': ['conjunção', 'coordenativa'], 'DET': 'determinante', 'INTJ' : 'interjeição', 'NOUN' : 'substantivo', 'NUM' : 'numeral', 'PART': 'partícula', 'PRON' : 'pronome', 'PROPN' : 'nome próprio', 'PUNCT' : 'pontuação', 'SCONJ' : 'conjunção subordinativa', 'SYM' : 'símbolo', 'VERB' : 'verbo', 'X' : 'outro'}
-
+nlp = spacy.load('pt_core_news_sm') 
 doc = nlp('A galinha atravessou a rua para ganhar um milhão.') #Coloque o texto aqui
 
-word_With_Class = {}
-for token in doc:
-    Class = token.tag_
-    word_With_Class[token.text] = class_pt[Class]
+def spacy(doc, nlp):
+    class_pt = {'ADJ': 'adjetivo', 'ADP' : 'aposição', 'ADV': 'adverbio', 'AUX': 'auxiliar', 'CCONJ': ['conjunção', 'coordenativa'], 'DET': 'determinante', 'INTJ' : 'interjeição', 'NOUN' : 'substantivo', 'NUM' : 'numeral', 'PART': 'partícula', 'PRON' : 'pronome', 'PROPN' : 'nome próprio', 'PUNCT' : 'pontuação', 'SCONJ' : 'conjunção subordinativa', 'SYM' : 'símbolo', 'VERB' : 'verbo', 'X' : 'outro'}
+    word_With_Class = {}
+    for token in doc:
+        Class = token.tag_
+        word_With_Class[token.text] = class_pt[Class]
+    return word_With_Class
 
-print(word_With_Class)
+print(spacy(doc, nlp))
